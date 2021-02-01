5b0d2d881bd1124ba695ec8d8e55c75f1a947f18
sandeep <sandeepblh@gmail.com>
2021-02-02 10:38:59 +1100

    initial commit

---
+++ src/main/java/org/apthletic/espneventscrawl/EspnEventsCrawlApplication.java
@@ -0,0 +1,13 @@
+package org.apthletic.espneventscrawl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspnEventsCrawlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EspnEventsCrawlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/apthletic/espneventscrawl/EspnEventsCrawlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.apthletic.espneventscrawl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspnEventsCrawlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
